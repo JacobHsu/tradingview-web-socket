de346b02937125ed602b98783bbc829eaa1ca347
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-05 18:12:34 +0800

    feat: api.huobi.pro

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -6,13 +6,44 @@
 </template>
 
 <script>
+import { apiGet } from "./api";
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      symbol: "",
+      symbolInfo: {},
+      symbolList: [],
+    };
+  },
+  methods: {
+    fetchSymbolList() {
+      apiGet("common_symbols").then((res) => {
+        if (!res || !res.data) {
+          return;
+        }
+        const list = [];
+        for (let i = 0; i < res.data.length; i++) {
+          if (res.data[i]["quote-currency"] === "usdt") {
+            list.push(res.data[i]);
+          }
+        }
+        const symbol = list.length ? list[0].symbol : "";
+        this.symbol = symbol;
+        this.symbolInfo = list[0];
+        this.symbolList = list;
+        console.log(symbol, list[0]) // dfusdt {...}
+      });
+    },
+  },
+  created() {
+    this.fetchSymbolList();
+  },
 }
 </script>
 
